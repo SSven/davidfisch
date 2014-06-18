d6ebd37bf5c7bcdab74e4781c8aad4bf106b8648
Jurek Barth <post@jurekbarth.de>
2014-06-18 12:39:45 +0200

    active menu fix

--- js/global.js
+++ js/global.js
@@ -1,3 +1,12 @@
+// menu toggler
 $('.menu-button').click(function (argument) {
   $('.menu').toggleClass('active');
 });
+
+// menu highlighting
+var pathname=window.location.pathname;
+// hacky
+if (pathname == "/work-single.php") {
+  pathname = "/work.php"
+}
+$('a[href$="'+pathname+'"]').addClass("active");
--- parts/menu.php
+++ parts/menu.php
@@ -1,13 +1,13 @@
 <header>
-  <h1 class="logo">David Fuchs</h1>
+  <a href="/"><h1 class="logo">David Fuchs</h1></a>
 
   <nav class="menu-wrapper">
     <button class="menu-button">☰</button>
     <ul class="menu">
       <li><a href="/">Home</a></li>
-      <li><a class="active" href="/work.php">Work</a></li>
-      <li><a href="/aboutme.php">About me</a></li>
-      <li><a href="/contact.php">Contact</a></li>
+      <li><a href="/work.php">Work</a></li>
+      <li><a href="#">About me</a></li>
+      <li><a href="#">Contact</a></li>
     </ul>
   </nav>
 </header>
